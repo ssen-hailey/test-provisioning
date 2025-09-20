ab158e8119b9db7cda64aaca1877e20b4820a0e0
seeun <seeun@yang-gyuhuiui-Macmini.local>
2025-09-21 00:55:38 +0100

    add jenkins sg,tg,lb

---
+++ terraform/platform/jenkins/seeun_id_apnortheast2/remote_state.tf
@@ -0,0 +1,12 @@
+data "terraform_remote_state" "kms_apne2" {
+  backend = "s3"
+  config  = merge(var.remote_state.kms.seeun-id.apne2)
+  #config = merge(var.remote_state.kms.seeun-id.apne2, {"assume_role"= { "role_arn" = var.assume_role_arn }})
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config  = merge(var.remote_state.vpc.tmcd_apnortheast2)
+  #config = merge(var.remote_state.vpc.tmcd_apnortheast2, {"assume_role"= { "role_arn" = var.assume_role_arn }})
+}
+
