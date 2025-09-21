3947f6b0f90de69b52427ffca253084b54fc9c1a
seeun <seeun@yang-gyuhuiui-Macmini.local>
2025-09-21 12:50:31 +0100

    jenkins_codebuild

---
+++ terraform/codebuild/seeun-id/seeun_id_apnortheast2/remote_states.tf
@@ -0,0 +1,17 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  # config = merge(var.remote_state.vpc.tmcd_apnortheast2)
+  config = merge(var.remote_state.vpc.sed_apnortheast2, { "assume_role" = { "role_arn" = var.assume_role_arn } })
+}
+
+data "terraform_remote_state" "iam" {
+  backend = "s3"
+  # config = merge(var.remote_state.iam.zerone-id)
+  config = merge(var.remote_state.iam.seeun-id, { "assume_role" = { "role_arn" = var.assume_role_arn } })
+}
+
+data "terraform_remote_state" "security_group" {
+  backend = "s3"
+  # config = merge(var.remote_state.security_group.zerone-id.tmcdapne2)
+  config = merge(var.remote_state.security_group.seeun-id.sedapne2, { "assume_role" = { "role_arn" = var.assume_role_arn } })
+}
