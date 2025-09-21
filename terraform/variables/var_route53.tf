6554bf8b18d4dc2887f65518c4cad80f9b08aa06
seeun <seeun@yang-gyuhuiui-Macmini.local>
2025-09-21 12:50:31 +0100

    jenkins_codebuild

--- terraform/variables/var_route53.tf
+++ terraform/variables/var_route53.tf
@@ -1,6 +1,6 @@
 variable "r53_variables" {
   default = {
     se_terraform_devart_tv_zone_id            = "Z09592541ORFQU165OP4F"
-    star_se_terraform_devart_tv_acm_apne2_arn = "arn:aws:acm:ap-northeast-2:066346343248:certificate/10d501d4-a8c8-4358-9996-98060a907e47"
+    star_se_terraform_devart_tv_acm_apne2_arn = "arn:aws:acm:ap-northeast-2:680463082671:certificate/464ee562-e62b-4f66-9856-008ca950f023"
   }
 }
