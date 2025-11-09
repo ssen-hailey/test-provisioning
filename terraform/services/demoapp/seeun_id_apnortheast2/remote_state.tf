6c6c8f1bf358ff39609a3632d60c9c51e5b2237b
Hailey <ssen.hailey@gmail.com>
2025-11-09 15:12:04 +0000

    ADD seeun_id

--- terraform/services/demoapp/seeun_id_apnortheast2/remote_state.tf
+++ terraform/services/demoapp/seeun_id_apnortheast2/remote_state.tf
@@ -3,3 +3,4 @@ data "terraform_remote_state" "vpc" {
   config = merge(var.remote_state.vpc.sed_apnortheast2)
   #config = merge(var.remote_state.vpc.sed_apnortheast2, { "assume_role" = { "role_arn" = var.assume_role_arn } })
 }
+ 
--- terraform/services/demoapp/seeun_id_apnortheast2/terraform.tfvars
+++ terraform/services/demoapp/seeun_id_apnortheast2/terraform.tfvars
@@ -1,1 +1,1 @@
-assume_role_arn          = "arn:aws:iam::680463082671:role/terraform-runner"
+assume_role_arn          = "arn:aws:iam::680463082671:role/terraform-runner" 
